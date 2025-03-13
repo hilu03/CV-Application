8ce9056df785bf24c8d837d531003e3864e1273e
Luong Tran <tranthihienluong2003@gmail.com>
2025-03-13 08:46:40 +0700

    update experience display

--- src/components/ExperienceDisplay.jsx
+++ src/components/ExperienceDisplay.jsx
@@ -1,12 +1,19 @@
+import "../styles/ExperienceDisplay.css";
+
 function ExperienceDisplay({info}) {
   return (
-    <div>
-      <h3>{info && info.company}</h3>
-      <p>{info && info.start} {info.end.length > 0 && ` - ${info.end}`}</p>
-      <p>{info && info.position}</p>
-      <ul>
-       {info.description && info.description.map(r => <li>{r}</li>)}
-      </ul>
+    <div className="experience-container">
+      <div className="left-side">
+        <h3 className="company">{info && info.company}</h3>
+        <p>{info && info.start} {info.end.length > 0 && ` - ${info.end}`}</p>
+      </div>
+      <div className="right-side">
+        <div className="dot"></div>
+        <p className="position">{info && info.position}</p>
+        <ul>
+          {info.description && info.description.map(r => <li>{r}</li>)}
+        </ul>
+      </div>
     </div>
   );
 }
